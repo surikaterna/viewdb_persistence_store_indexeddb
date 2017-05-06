bc1b017996e0c85c9a85bca61b406795e94e2358
Andreas Karlsson <sprawl@onlajn.nu>
2017-05-06 12:51:46 +0200

    switched to fake-indexeddb and fixed tests

--- test/util.js
+++ test/util.js
@@ -1,15 +1,5 @@
-var sqlite3 = require('sqlite3').verbose();
-var indexeddbjs = require('indexeddb-js');
+var indexeddbjs = require('fake-indexeddb');
 
 module.exports = function() {
-
-	var engine    = new sqlite3.Database(':memory:');
-	var scope     = indexeddbjs.makeScope('sqlite3', engine);
-	return scope.indexedDB;
-}
-
-/*
-module.exports = function() {
-	return window.indexedDB;
-}
-*/
+	return indexeddbjs;
+};
